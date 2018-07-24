15a1ec17452946d28d74a0f79b123bfd3cd86f50
josh Bergstrom <joshBergstrom463@gmail.com>
2018-07-24 16:26:16 -0700

    worked on ui

---
+++ Fooder/Fooder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fooder
+//
+//  Created by Josh Bergstrom on 7/24/18.
+//  Copyright © 2018 Josh Bergstrom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
