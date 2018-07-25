755e02d0114d416513a20818bb1996529976d1f7
josh Bergstrom <joshBergstrom463@gmail.com>
2018-07-25 11:12:12 -0700

    worked on adding json

---
+++ Fooder/Fooder/Network Services/NetworkManager.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkManager.swift
+//  Fooder
+//
+//  Created by Josh Bergstrom on 7/25/18.
+//  Copyright © 2018 Josh Bergstrom. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkManager{
+    
+    static func getRestaurent(){
+        
+    }
+}
---
+++ Fooder/Fooder/classes /restaurant.swift
@@ -0,0 +1,21 @@
+//
+//  restaurant.swift
+//  Fooder
+//
+//  Created by Josh Bergstrom on 7/25/18.
+//  Copyright © 2018 Josh Bergstrom. All rights reserved.
+//
+
+import Foundation
+
+var restaurants = [Restaurant]()
+
+class Restaurant  {
+    var name = 
+}
+
+
+
+
+
+
