b83a88cb4567dec908859dbeab75b6537a05c5ff
josh Bergstrom <joshBergstrom463@gmail.com>
2018-07-25 16:36:24 -0700

    stared adding almofireImage

--- Fooder/Fooder/ViewController.swift
+++ Fooder/Fooder/ViewController.swift
@@ -6,20 +6,53 @@
 //  Copyright © 2018 Josh Bergstrom. All rights reserved.
 //
 
+
+
+// view controller for the main tinder like page 
 import UIKit
+import SwiftyJSON
+import Alamofire
+
 
 class ViewController: UIViewController {
+    var foodsearched = String()
+    
 
+    
+    @IBOutlet weak var MainImage: UIImageView!
+    @IBOutlet weak var RatingImage: UIImageView!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var storeName: UILabel!
+    
+    var MainImageUrl = "https://s3-media2.fl.yelpcdn.com/bphoto/7AJax0lwL78aRorRGjvw8A/o.jpg"                                   //this will be used to find the image Update later
+    var rating = Double() // this will be used for the rating image
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // im connecting all the api info
+        let apiToContact = "https://api.yelp.com/v3/businesses/search?term=\(foodsearched)&latitude=37.786882&longitude=-122.399972"
+        Alamofire.request(apiToContact).validate().responseJSON() { response in
+            switch response.result {
+            case .success:
+                if let value = response.result.value {
+                    let json = JSON(value)
+                    let restaurant = Restaurant(json: json)
+                    
+                    
+                }
+            case .failure(let error):
+                print(error)
+            }
+        
+        }
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
 }
+
 
--- Fooder/Fooder/classes /restaurant.swift
+++ Fooder/Fooder/classes /restaurant.swift
@@ -7,12 +7,29 @@
 //
 
 import Foundation
+import SwiftyJSON
 
-var restaurants = [Restaurant]()
 
-class Restaurant  {
-    var name = 
+struct Restaurant {
+    let name: String
+    let imageURL: String
+    let rating: Double
+    let distance: Double
+    let categories: String
+    let address: String
+    let ID: String
+    
+    init(json: JSON) {
+        self.name = json["businesses"][0]["name"].stringValue
+        self.imageURL = json["businesses"][0]["image_url"].stringValue
+        self.rating = json["businesses"][0]["rating"].doubleValue
+        self.categories = json["businesses"][0]["categories"]["title"].stringValue
+        self.distance = json["businesses"][0]["distance"].doubleValue
+        self.address = json["businesses"][0]["display_address"].stringValue
+        self.ID = json["businesses"][0]["id"].stringValue
+    }
 }
+
 
 
 
